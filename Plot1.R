957843f652683fca507e1b3200db9c69dd42041b
RayOrtilla <89635595+RayOrtilla@users.noreply.github.com>
2022-04-21 12:01:14 +0800

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,25 +1,25 @@
-temp <- tempfile()
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-power <- read.table(unz(temp,"household_power_consumption.txt"), 
-                    sep=";", 
-                    header = T, 
-                    na="?", 
-                    colClasses = c("character",
-                                   'character',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric'))
+library("data.table")
 
-unlink(temp)
-power <- power[which(power$Date == '2/2/2007' | power$Date=='1/2/2007'),]
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
 
-power$POSIX <-as.POSIXlt.character(paste(power$Date,power$Time),format = "%d/%m/%Y %H:%M:%S")
+#Reads in data from file then subsets data for specified dates
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+                             )
 
-#plot.1
-png(filename="plot1.png",width=480, height=480)
-hist(power$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
+# Prevents histogram from printing in scientific notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+# Change Date Column to Date Type
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+
+png("plot1.png", width=480, height=480)
+
+## Plot 1
+hist(powerDT[, Global_active_power], main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
 dev.off()
